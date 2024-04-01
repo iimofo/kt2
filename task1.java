7adbf6fd69381a74e78b71738917cbf36b60c927
iimofo <61416424+iimofo@users.noreply.github.com>
2024-04-01 22:45:00 +0500

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        int index = haystack.indexOf(needle);
+        System.out.println(index);
+    }
+}
